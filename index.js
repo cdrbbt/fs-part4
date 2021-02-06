f78f25660b24c705786bc07369e77082225fecb2
cdrbbt <egor.bykadorov@gmail.com>
2021-02-06 23:51:24 +0200

    4.2 module seperation

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
-const app = express()
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
+const app = require('./app')
 
 const PORT = config.PORT
-app.listen(PORT, () => {
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
   logger.info(`server running on port ${PORT}`)
 })
