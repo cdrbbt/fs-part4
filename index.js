3e44591f5548d9bca77dd99c802af5a6d17dae6a
cdrbbt <egor.bykadorov@gmail.com>
2021-02-06 23:41:19 +0200

    4.1.4 extrated routes to controller filer

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
 const config = require('./utils/config')
 const http = require('http')
-const express =require('express')
-const app = express()
+const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const app = express()
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (req, res) => {
-  Blog
-  .find({})
-  .then(blogs => {
-    res.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog
-  .save()
-  .then(result => {
-    res.status(201).json(result)
-  })
-})
 
 const PORT = config.PORT
 app.listen(PORT, () => {
