a2e294e7caa46f052e5d65cf8be1f79eee36c0ed
cdrbbt <egor.bykadorov@gmail.com>
2021-02-07 11:12:27 +0200

    4.3 dummy test

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,20 +3,20 @@ const Blog = require('../models/blog')
 
 blogsRouter.get('/', (req, res) => {
   Blog
-  .find({})
-  .then(blogs => {
-    res.json(blogs)
-  })
+    .find({})
+    .then(blogs => {
+      res.json(blogs)
+    })
 })
 
 blogsRouter.post('/', (req, res) => {
   const blog = new Blog(req.body)
 
   blog
-  .save()
-  .then(result => {
-    res.status(201).json(result)
-  })
+    .save()
+    .then(result => {
+      res.status(201).json(result)
+    })
 })
 
 module.exports = blogsRouter
