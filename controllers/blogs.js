96a27e455b60c4eaad869986fe1c8f0ad180a2d0
cdrbbt <egor.bykadorov@gmail.com>
2021-02-08 18:49:15 +0200

    4.14 blog deletion and update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,4 +12,15 @@ blogsRouter.post('/', async (req, res) => {
   res.status(201).json(result)
 })
 
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id)
+  res.status(204).end()
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, {likes: req.body.likes}, {new: true})
+
+  res.json(updatedBlog)
+})
+
 module.exports = blogsRouter
